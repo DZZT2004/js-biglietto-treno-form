1e0e1816cd169ec10be827d4f551f658b5233605
DZZT2004 <taodaiuwu@gmail.com>
2022-10-23 23:07:55 +0200

    milestone 1

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var kilometri = prompt("Inserisci i kilometri che vuoi percorrere");
-var età = prompt("Inserisci la tua età");
-var prezzo_non_scontato = kilometri * 0.21;
-var prezzo_scontato = 0;
-if(età < 18){
+const sub = document.getElementById("submit");
+const nomeCognome = document.getElementById("nomeCognome");
+const km = document.getElementById("km");
+const eta = document.getElementById("età");
+sub.addEventListener('click', 
+    function(){
+        var prezzo_non_scontato = km.value * 0.21;
+        var prezzo_scontato = 0;
+        if(eta.value == "minorenne"){
+            prezzo_scontato = (prezzo_non_scontato/100) * 80;
+            console.log("Il prezzo scontato è " + prezzo_scontato.toFixed(2) + " euro");
+        }else if(eta.value == "over65"){
+            prezzo_scontato = (prezzo_non_scontato/100) * 60;
+            console.log("Il prezzo scontato è " + prezzo_scontato.toFixed(2) + " euro");
+        }else if(eta.value == "maggiorenne"){
+            console.log(prezzo_non_scontato + " euro");
+        }
+        console.log("ciao");
+        console.log(nomeCognome.value);
+        console.log(km.value);
+        console.log(eta.value);
+        }
+    )
+
+
+/*if(età < 18){
     prezzo_scontato = (prezzo_non_scontato/100) * 80;
     console.log("Il prezzo scontato è " + prezzo_scontato.toFixed(2));
 }else if(età >= 65){
@@ -10,4 +31,4 @@ if(età < 18){
     console.log("Il prezzo scontato è " + prezzo_scontato.toFixed(2));
 }else{
     console.log(prezzo_non_scontato);
-}
+}*/
